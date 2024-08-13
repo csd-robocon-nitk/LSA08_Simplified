a3a831b6209cb91c260a44e673e1961a0fc24e61
JoelJojoP <joeljojop@gmail.com>
2024-08-14 01:18:36 +0530

    Add library template files

---
+++ lsa08_simplified.cpp
@@ -0,0 +1,13 @@
+/**
+ * Source code of the LSA08_Simplified library
+ * Developed by CSD Robocon NITK
+ * 
+ * This is free software. You can redistribute it and/or modify it under
+ * the terms of MIT Licence.
+ * To view a copy of this license, visit http://opensource.org/licenses/mit-license.php
+ * 
+ * version: 1.0.0
+ */
+
+#include "lsa08_simplified.h"
+
---
+++ lsa08_simplified.h
@@ -0,0 +1,12 @@
+/**
+ * Header for LSA08_Simplified library
+ * Developed by CSD Robocon NITK
+ * 
+ * This is free software. You can redistribute it and/or modify it under
+ * the terms of MIT Licence.
+ * To view a copy of this license, visit http://opensource.org/licenses/mit-license.php
+ * 
+ * version: 1.0.0
+ */
+
+#include<Arudino.h>
