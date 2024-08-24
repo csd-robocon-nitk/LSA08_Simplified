fb7db54ad9aa49ba85f95a084e232617b91d50e5
JoelJojoP <joeljojop@gmail.com>
2024-08-24 12:53:24 +0530

    Add constructor for LSA08 class

--- lsa08_simplified.cpp
+++ lsa08_simplified.cpp
@@ -11,3 +11,36 @@
 
 #include "lsa08_simplified.h"
 
+LSA08::LSA08(lsa08_mode mode, uint8_t pin)
+{
+    this->mode = mode;
+    this->pin = pin;
+    if(mode == LSA08_MODE_DIGITAL)
+    {
+        pinMode(pin, INPUT);
+    }
+}
+
+LSA08::LSA08(lsa08_mode mode, HardwareSerial *port, int baudrate)
+{
+    this->mode = mode;
+    if(mode == LSA08_MODE_SERIAL)
+    {
+        this->port = port;
+        port->begin(baudrate);
+    }
+}
+
+int8_t LSA08::send_packet(unsigned char addr, unsigned char command, unsigned char value, com_type type)
+{
+    if(mode != LSA08_MODE_SERIAL)
+        return MODE_ERROR;
+    else
+    {
+        port->write(addr);
+        port->write(command);
+        port->write(value);
+        port->write(addr + command + value);
+        return 0;
+    }
+}
--- lsa08_simplified.h
+++ lsa08_simplified.h
@@ -9,4 +9,31 @@
  * version: 1.0.0
  */
 
-#include<Arudino.h>
+#include<Arduino.h>
+
+#define MODE_ERROR -1
+
+typedef enum lsa08_modes
+{
+    LSA08_MODE_ANALOG,
+    LSA08_MODE_DIGITAL,
+    LSA08_MODE_SERIAL
+} lsa08_mode;
+
+typedef enum com_types
+{
+    COM_TYPE_COMMAND,
+    COM_TYPE_DATA
+} com_type;
+
+class LSA08
+{
+private:
+    uint8_t pin;
+    HardwareSerial *port;
+    lsa08_mode mode;
+    int8_t send_packet(unsigned char addr, unsigned char command, unsigned char value, com_type type);
+public:
+    LSA08(lsa08_mode mode, uint8_t pin);
+    LSA08(lsa08_mode mode, HardwareSerial *port, int baudrate);
+};
